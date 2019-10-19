0aa842c1527d5d5af099f9a9a75416d1a2f6c705
MeSKiL <403797970@qq.com>
2019-10-19 20:41:41 +0800

    first commit

---
+++ vue/src/core/global-api/index.js
@@ -0,0 +1,65 @@
+/* @flow */
+
+import config from '../config'
+import {initUse} from './use'
+import {initMixin} from './mixin'
+import {initExtend} from './extend'
+import {initAssetRegisters} from './assets'
+import {set, del} from '../observer/index'
+import {ASSET_TYPES} from 'shared/constants'
+import builtInComponents from '../components/index'
+
+import {
+    warn,
+    extend,
+    nextTick,
+    mergeOptions,
+    defineReactive
+} from '../util/index'
+
+export function initGlobalAPI(Vue: GlobalAPI) { // 给vue增加了各种api
+    // config
+    const configDef = {}
+    configDef.get = () => config // configDef = config
+    if (process.env.NODE_ENV !== 'production') {
+        configDef.set = () => {
+            warn(
+                'Do not replace the Vue.config object, set individual fields instead.'
+            )
+        }
+    }
+    Object.defineProperty(Vue, 'config', configDef) //vue.config = configDef
+
+    // exposed util methods.
+    // NOTE: these are not considered part of the public API - avoid relying on
+    // them unless you are aware of the risk.
+    Vue.util = { // 不稳定不建议使用
+        warn,
+        extend,
+        mergeOptions,
+        defineReactive
+    }
+
+    Vue.set = set
+    Vue.delete = del
+    Vue.nextTick = nextTick
+
+    Vue.options = Object.create(null)
+    ASSET_TYPES.forEach(type => {
+        // 'component',
+        // 'directive',
+        // 'filter'
+        Vue.options[type + 's'] = Object.create(null)
+    }) // 挂载到Vue.options下
+
+    // this is used to identify the "base" constructor to extend all plain-object
+    // components with in Weex's multi-instance scenarios.
+    Vue.options._base = Vue // base指向vue
+
+    extend(Vue.options.components, builtInComponents) // 将keep-alive扩展到components下
+
+    initUse(Vue) // 定义Vue.init
+    initMixin(Vue) // Vue.mixin
+    initExtend(Vue) // Vue.extend
+    initAssetRegisters(Vue)
+}
---
+++ vue/src/core/instance/index.js
@@ -0,0 +1,24 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+function Vue (options) { // 代码清晰明了
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  this._init(options)
+}
+
+// 往vue原型上挂载方法
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
